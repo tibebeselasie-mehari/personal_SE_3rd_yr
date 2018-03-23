bc32383af8948de0b6ceeb957e9ae48061752220
tibebeselasie-mehari <37696850+tibebeselasie-mehari@users.noreply.github.com>
2018-03-23 22:36:09 +0300

    added a basic CLI support

--- Pipe-and-filter-example/duplication_checker.py
+++ Pipe-and-filter-example/duplication_checker.py
@@ -1,20 +1,25 @@
-from filters import *
-
-DIR_PATH = './sample_texts/'
+import os
+from filters import filter_files, read_files, hash_files, frequency_filter, duplicate_files_filter, show_report
+from sys import argv
 
 def main():
+    dir_path = './sample_texts/'
+
+    if len(argv) > 1:
+        dir_path = argv[1]
+
     # Source (files in the current directory)
-    source = os.listdir(DIR_PATH)
+    source = os.listdir(dir_path)
 
     # the files are filtered by extension and piped to next filter
     stream1 = filter_files(source, extension=".txt")
 
     # their data is read and piped to next filter
-    stream2 = read_files(stream1, dir_path = DIR_PATH)
+    stream2 = read_files(stream1, dir_path=dir_path)
 
     # md5 hash for their content is calculated and piped
     stream3 = hash_files(stream2)
-    
+
     # files with similiar hashes are categorized together and piped
     stream4 = frequency_filter(stream3)
 
@@ -23,6 +28,7 @@ def main():
 
     # a report is generated (sink)
     show_report(stream5)
+
 
 if __name__ == '__main__':
     main()
--- Pipe-and-filter-example/filters.py
+++ Pipe-and-filter-example/filters.py
@@ -1,17 +1,20 @@
-import os, hashlib
+import os
+import hashlib
 from functools import reduce
+from print_funcs import print_header, print_indented, print_line, print_underlined
 
-from print_funcs import *
 
 def read_files(filenames, dir_path, encoding="utf-8"):
-    ''' Generator that yields data from filenames as (filename, data) tuple '''    
+    ''' Generator that yields data from filenames as (filename, data) tuple '''
     for filename in filenames:
-        yield filename, open("/".join([dir_path,filename]), 'r', encoding="utf-8").read()
+        yield filename, open("/".join([dir_path, filename]), 'r', encoding="utf-8").read()
+
 
 def hash_files(inputs):
     ''' Generator that hashes the content of each files in its inputs '''
     for filename, data in inputs:
         yield filename, data, hashlib.md5(data.encode("utf-8")).hexdigest()
+
 
 def frequency_filter(inputs):
     ''' a filter that packs filenames that contain the same hash as together and pipe it to the next filter '''
@@ -23,11 +26,12 @@ def frequency_filter(inputs):
             frequency_table[hash]['files'] += [filename]
         except KeyError:
             frequency_table[hash] = {
-                'content' : data,
-                'files' : [filename]
+                'content': data,
+                'files': [filename]
             }
 
     return frequency_table
+
 
 def duplicate_files_filter(inputs):
     ''' Generator that yields filenames that contains the same hash as one '''
@@ -36,31 +40,35 @@ def duplicate_files_filter(inputs):
         if len(data['files']) > 1:
             yield list(data.values())
 
-def filter_files(inputs, extension = ""):
+
+def filter_files(inputs, extension=""):
     ''' Filter inputs stream according to a pattern '''
     for item in inputs:
         if item.endswith(extension):
             yield item
 
+
 def show_report(inputs):
     inputs = list(inputs)
 
     print_header()
-    
-    totals_counter = reduce(lambda x, y: len(list(x)[1]) + len(list(y)[1]), inputs)
-    
+
+    totals_counter = reduce(lambda x, y: len(
+        list(x)[1]) + len(list(y)[1]), inputs)
+
     print_underlined("Overall summary:", indentation=1)
 
-    print_indented("<{:.>4}> file contents".format(len(inputs)), indentation = 2)
-    print_indented("<{:.>4}> files are found to be duplicate.".format(totals_counter), indentation = 2, end="\n\n")
+    print_indented("<{:.>4}> file contents".format(len(inputs)), indentation=2)
+    print_indented("<{:.>4}> files are found to be duplicate.".format(
+        totals_counter), indentation=2, end="\n\n")
 
     print_underlined("Detailed Results:", end="\n", indentation=1)
 
     for content, files in inputs:
-        print_indented("[+] Content : '", content[:50], end=" ... '\n", indentation = 2)
-        print_indented("[+] <%d> files share the above content: "%(len(list(files))), indentation = 2)
+        print_indented("[+] Content : '", content[:50],
+                       end=" ... '\n", indentation=2)
+        print_indented("[+] <%d> files share the above content: " %
+                       (len(list(files))), indentation=2)
         for each_file in files:
-            print_indented("\t [+] %s"%each_file, indentation = 3)
-        print_line(50, symbol = "-", indentation = 3, top="\n", bottom = "\n")
-
-
+            print_indented("\t [+] %s" % each_file, indentation=3)
+        print_line(50, symbol="-", indentation=3, top="\n", bottom="\n")
